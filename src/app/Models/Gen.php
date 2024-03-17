c4e708e477a5fdd6cd505fbe8ff8d2ee872399b1
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-03-17 22:59:11 +0900

    add models Gen, Summary

---
+++ src/app/Models/Gen.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Gen
+ *
+ * @property string|null $TITLE
+ * @property string|null $ARTIST
+ * @property string|null $ALBUM
+ * @property Carbon|null $YEAR
+ * @property string|null $CIRCLE
+ * @property int|null    $DURATION
+ * @property Carbon|null $SONGSTART
+ * @property Carbon|null $SONGEND
+ * @property int         $SONGID
+ * @property int         $ALBUMID
+ * @property string|null $CIRCLELINK
+ * @property float|null  $RATING
+ * @property int|null    $TIMESRATED
+ * @property int|null    $LISTENERS
+ * @property Carbon      $LASTUPDATE
+ * @property string|null $DEBUG
+ * @property string|null $ALBUMART
+ */
+class Gen extends Model
+{
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $table = 'gen1';
+    protected $primaryKey = 'LASTUPDATE';
+
+    protected $casts = [
+        'YEAR' => 'datetime',
+        'DURATION' => 'int',
+        'SONGSTART' => 'datetime',
+        'SONGEND' => 'datetime',
+        'SONGID' => 'int',
+        'ALBUMID' => 'int',
+        'RATING' => 'float',
+        'TIMESRATED' => 'int',
+        'LISTENERS' => 'int',
+        'LASTUPDATE' => 'datetime',
+    ];
+
+    protected $fillable = [
+        'TITLE',
+        'ARTIST',
+        'ALBUM',
+        'YEAR',
+        'CIRCLE',
+        'DURATION',
+        'SONGSTART',
+        'SONGEND',
+        'SONGID',
+        'ALBUMID',
+        'CIRCLELINK',
+        'RATING',
+        'TIMESRATED',
+        'LISTENERS',
+        'DEBUG',
+        'ALBUMART',
+    ];
+}
---
+++ src/app/Models/NowPlaying.php
@@ -0,0 +1,57 @@
+<?php declare(strict_types=1);
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class NowPlaying
+ *
+ * @property string|null $TITLE
+ * @property string|null $ARTIST
+ * @property string|null $ALBUM
+ * @property string|null $YEAR
+ * @property string|null $CIRCLE
+ * @property int|null    $DURATION
+ * @property Carbon|null $SONGEND
+ * @property float|null  $RATING
+ * @property string|null $ALBUMART
+ * @property int|null    $IN_WEEK
+ * @property int|null    $IN_MONTH
+ * @property int|null    $IN_YEAR
+ */
+class NowPlaying extends Model
+{
+    public $incrementing = false;
+    public $timestamps = false;
+    protected $table = 'nowPlaying';
+
+    protected $casts = [
+        'DURATION' => 'int',
+        'SONGEND' => 'datetime',
+        'RATING' => 'float',
+        'IN_WEEK' => 'int',
+        'IN_MONTH' => 'int',
+        'IN_YEAR' => 'int',
+    ];
+
+    protected $fillable = [
+        'TITLE',
+        'ARTIST',
+        'ALBUM',
+        'YEAR',
+        'CIRCLE',
+        'DURATION',
+        'SONGEND',
+        'RATING',
+        'ALBUMART',
+        'IN_WEEK',
+        'IN_MONTH',
+        'IN_YEAR',
+    ];
+}
