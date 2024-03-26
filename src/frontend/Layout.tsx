c3b93b649e51c0518d2fd782e73e8b766094346a
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-03-27 00:08:16 +0900

    サイトタイトルのセンタリング位置修正、縦スクロールバーが引っかかる問題を修正
    Header
    - サイトタイトルがきちんと真ん中にセンタリングされるように修正
      - ダークモードボタンをabsoluteにして絶対配置としflexから除外
    
    Layout
    - iPhone実機でアドレスバーが干渉して縦スクロールバーが引っかかる問題を修正

--- src/frontend/Layout.tsx
+++ src/frontend/Layout.tsx
@@ -3,7 +3,7 @@ import { NowPlaying } from "./NowPlaying/NowPlaying";
 
 export function Layout() {
   return (
-    <div className="flex h-screen w-full flex-col">
+    <div className="flex h-dvh w-full flex-col">
       <Header />
       <div className="h-full overflow-y-auto px-4 py-8">
         <NowPlaying />
