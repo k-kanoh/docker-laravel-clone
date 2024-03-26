8f7bc04568b64f2e9afafd7fe75450417a501d96
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-03-27 00:08:16 +0900

    サイトタイトルのセンタリング位置修正、縦スクロールバーが引っかかる問題を修正
    Header
    - サイトタイトルがきちんと真ん中にセンタリングされるように修正
      - ダークモードボタンをabsoluteにして絶対配置としflexから除外
    
    Layout
    - iPhone実機でアドレスバーが干渉して縦スクロールバーが引っかかる問題を修正

--- src/frontend/Header.tsx
+++ src/frontend/Header.tsx
@@ -2,11 +2,13 @@ import { ModeToggle } from "@/components/DarkMode/mode-toggle";
 
 export function Header() {
   return (
-    <header className="flex h-max items-center justify-between border-b bg-gray-100 dark:bg-gray-800">
-      <div className="mx-auto font-serif text-2xl tracking-widest drop-shadow-[0_2px_3px_rgba(64,64,64,1)] sm:mx-4 sm:text-4xl sm:font-semibold dark:text-white dark:drop-shadow-[0_2px_3px_rgba(192,192,192,1)]">
-        砕月
+    <header className="relative min-h-14 border-b bg-gray-100 dark:bg-gray-800">
+      <div className="flex size-full items-center justify-center px-4 sm:justify-start">
+        <div className="font-serif text-2xl tracking-widest drop-shadow-[0_2px_3px_rgba(64,64,64,1)] sm:text-4xl sm:font-semibold dark:text-white dark:drop-shadow-[0_2px_3px_rgba(192,192,192,1)]">
+          砕月
+        </div>
       </div>
-      <div className="m-2 sm:m-4">
+      <div className="absolute right-2 top-1/2 -translate-y-1/2">
         <ModeToggle />
       </div>
     </header>
