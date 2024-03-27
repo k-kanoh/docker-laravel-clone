16970255fa621d9a7f49c8c8ddb8f660d9910cfb
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-03-27 21:21:57 +0900

    pagination実装
    - shadcn-ui/pagination
    - ページ切り替え時に縦スクロールバーの位置をリセット

--- src/frontend/components/ui/pagination.tsx
+++ src/frontend/components/ui/pagination.tsx
@@ -47,7 +47,7 @@ const PaginationLink = ({
 }: PaginationLinkProps) => (
   <a
     aria-current={isActive ? "page" : undefined}
-    className={cn(
+    className={cn("cursor-pointer",
       buttonVariants({
         variant: isActive ? "outline" : "ghost",
         size,
@@ -70,7 +70,7 @@ const PaginationPrevious = ({
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <span>前</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = "PaginationPrevious"
@@ -85,7 +85,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
+    <span>次</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 )
