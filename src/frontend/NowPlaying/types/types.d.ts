ac714fda5ba02e2def5c20c8f87fcdcabee59d62
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-04-07 16:10:51 +0900

    再生中のプログレス表示を実装

--- src/frontend/NowPlaying/types/types.d.ts
+++ src/frontend/NowPlaying/types/types.d.ts
@@ -1,12 +1,19 @@
 interface GenType {
+  ID: number;
   TITLE: string;
   ARTIST: string;
   ALBUM: string;
   YEAR: string;
   CIRCLE: string;
   DURATION: number;
+  SONGSTART: Date;
   SONGEND: Date;
+  SONGID: number;
+  ALBUMID: number;
   RATING: number;
+  TIMESRATED: number;
+  LISTENERS: number;
+  LASTUPDATE: string;
   ALBUMART: string | null;
   IN_WEEK: number;
   IN_MONTH: number;
