63fca0b510a8230b05abfad1d24fc8cf7f7ef1e6
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2024-11-09 18:33:37 +0900

    LaravelとReactのプロジェクトを再構築、分離

---
+++ frontend/gen/src/main.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Layout } from "./Layout";
+import { ThemeProvider } from "./components/DarkMode/theme-provider";
+
+import "@/utils/dateExtensions";
+import "./main.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Layout />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Traits\CustomSerializeDate;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use CustomSerializeDate;
     use HasApiTokens;
     use HasFactory;
     use Notifiable;
