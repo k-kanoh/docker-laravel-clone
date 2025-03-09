a7f4a7556927578727c30ad80665e71f0c5255f9
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2025-03-09 14:27:06 +0900

    演奏終了後のフェッチで曲目が更新されていなかった場合は10秒後に再フェッチするように対応/他
    - app.bladeにTITLEを設定できるように修正

--- frontend/gen/src/NowPlaying/hooks/useTopGenApiQuery.ts
+++ frontend/gen/src/NowPlaying/hooks/useTopGenApiQuery.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { addSeconds } from "date-fns";
 import { fetchGenApi } from "@/lib/api/genApi";
@@ -6,13 +6,19 @@ import { fetchGenApi } from "@/lib/api/genApi";
 export function useTopGenApiQuery() {
   const [listeners, setListeners] = useState<number | null>(0);
 
-  const { data, isSuccess } = useQuery<GenApiResponseType>({
+  const { data, isSuccess, isFetching } = useQuery<GenApiResponseType>({
     queryKey: ["genApiRes", false, 1],
     queryFn: () => fetchGenApi(1, false),
     staleTime: 10 * 60 * 1000,
   });
 
+  const isFetchingRef = useRef(false);
+
   const queryClient = useQueryClient();
+
+  useEffect(() => {
+    isFetchingRef.current = isFetching;
+  }, [isFetching]);
 
   useEffect(() => {
     if (isSuccess && data.data[0]) {
@@ -21,14 +27,26 @@ export function useTopGenApiQuery() {
       const reload = addSeconds(new Date(data.data[0].SONGEND), 5);
       const reloadRemaining = reload.valueOf() - new Date().valueOf();
 
-      if (reloadRemaining > 0) {
-        const timer = setTimeout(
-          () => queryClient.invalidateQueries(),
-          reloadRemaining
-        );
+      let timer: NodeJS.Timeout;
 
-        return () => clearTimeout(timer);
+      if (reloadRemaining > 0) {
+        timer = setTimeout(() => {
+          if (!isFetchingRef.current) {
+            queryClient.invalidateQueries();
+          }
+        }, reloadRemaining);
+      } else {
+        timer = setInterval(() => {
+          if (!isFetchingRef.current) {
+            queryClient.invalidateQueries();
+          }
+        }, 10 * 1000);
       }
+
+      return () => {
+        clearTimeout(timer);
+        clearInterval(timer);
+      };
     }
 
     return;
